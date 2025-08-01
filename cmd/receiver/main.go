fa77e2248b1420ec3676fe772c92ea639826856f
nikolay <n.smolov@yandex.ru>
2025-08-01 21:57:30 +0300

    adds kafka cluster and init topit by producer

--- cmd/receiver/main.go
+++ cmd/receiver/main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"context"
+	"os/signal"
+	"syscall"
+
+	"github.com/niksmo/receipt/internal/receiver/adapters"
+	"github.com/niksmo/receipt/pkg/logger"
+)
+
+var (
+	seedBrokers = []string{
+		"127.0.0.1:19094",
+		"127.0.0.1:29094",
+		"127.0.0.1:29094",
+	}
+	topic    = "mail-receipt"
+	logLevel = "debug"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT,
+	)
+	defer cancel()
+
+	logger := logger.New(logLevel)
+
+	producer, err := adapters.NewMessageProducer(
+		ctx, logger, seedBrokers, topic,
+	)
+	if err != nil {
+		logger.Error().Err(err).Send()
+		return
+	}
+	defer producer.Close()
+}
---
+++ internal/receiver/adapters/kafka_broker_producer.go
@@ -0,0 +1,79 @@
+package adapters
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/niksmo/receipt/pkg/logger"
+	"github.com/twmb/franz-go/pkg/kadm"
+	"github.com/twmb/franz-go/pkg/kerr"
+	"github.com/twmb/franz-go/pkg/kgo"
+)
+
+const (
+	produceRetries    = 3
+	maxBatchSize      = 200 * 1024
+	partitions        = 3
+	replicationFactor = 3
+)
+
+var minInsyncReplicas = "2"
+
+type MessageProducer struct {
+	log   logger.Logger
+	kcl   *kgo.Client
+	topic string
+}
+
+func NewMessageProducer(
+	ctx context.Context, log logger.Logger, seedBrokers []string, topic string,
+) (MessageProducer, error) {
+	kcl, err := kgo.NewClient(
+		kgo.SeedBrokers(seedBrokers...),
+		kgo.DefaultProduceTopic(topic),
+		kgo.RequiredAcks(kgo.AllISRAcks()),
+		kgo.RecordRetries(produceRetries),
+		kgo.ProducerBatchMaxBytes(maxBatchSize),
+	)
+	if err != nil {
+		return MessageProducer{}, err
+	}
+
+	p := MessageProducer{log, kcl, topic}
+	if err := p.initTopic(ctx); err != nil {
+		return MessageProducer{}, err
+	}
+
+	return p, nil
+}
+
+func (p MessageProducer) ProduceReceipt() {}
+
+func (p MessageProducer) Close() {
+	const op = "MessageProducer.Close"
+	log := p.log.WithOp(op)
+
+	log.Info().Msg("closing producer")
+	p.kcl.Close()
+	log.Info().Msg("producer is closed")
+}
+
+func (p MessageProducer) initTopic(ctx context.Context) error {
+	const op = "MessageProvider.initTopic"
+
+	_, err := kadm.NewClient(p.kcl).CreateTopic(
+		ctx,
+		partitions,
+		replicationFactor,
+		map[string]*string{"min.insync.replicas": &minInsyncReplicas},
+		p.topic,
+	)
+	if err != nil {
+		if errors.Is(err, kerr.TopicAlreadyExists) {
+			return nil
+		}
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
